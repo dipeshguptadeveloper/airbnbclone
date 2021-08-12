7fa0c3d79f2816ba472138f92bfa0d387e4be4fe
Dipesh <dipesh@Dipeshs-MacBook-Air.local>
2021-08-12 20:56:40 +0530

    datepicker added

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,54 @@ import {
   SearchIcon,
   GlobeAltIcon,
   MenuIcon,
-  UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_Bélo.svg/800px-Airbnb_Logo_Bélo.svg.png"
           layout="fill"
@@ -20,9 +61,11 @@ function Header() {
       {/* middle section */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 outline-none bg-transparent text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -33,9 +76,50 @@ function Header() {
         <GlobeAltIcon className="h-6 cursor-pointer hidden sm:inline-flex" />
         <div className="flex sm:border-2 p-2  rounded-full cursor-pointer space-x-2 items-center">
           <MenuIcon className="h-6 hidden sm:inline-flex" />
-          <UserCircleIcon className="h-6" />
+          <UsersIcon className="h-6" />
         </div>
       </div>
+
+      {/* calender */}
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#fd5b61"]}
+            onChange={handleSelect}
+          />
+
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button
+              onClick={resetInput}
+              className="flex-grow text-gray-500 cursor-pointer"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={search}
+              className="flex-grow text-red-400 cursor-pointer"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
